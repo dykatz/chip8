a507461eee11956038bfae4a1a58d0bee4bad638
Dylan Katz <muddmaker.97@gmail.com>
2019-01-14 16:48:40 -0800

    improvements

--- chip8.c
+++ chip8.c
@@ -11,17 +11,18 @@ struct chip8_state {
 	uint16_t I;
 	uint8_t  gfx[64 * 32];
 	uint16_t stack[16];
+	uint8_t  sp;
 	uint8_t  key[16];
 	uint16_t delay;
 	uint16_t sound;
 };
 
 static void chip8_init(struct chip8_state *, const char *);
+static void chip8_draw(struct chip8_state *, SDL_Renderer *);
 
 int
 main(int argc, char **argv)
 {
-	uint16_t opcode;
 	struct chip8_state state;
 
 	SDL_Event ev;
@@ -34,7 +35,7 @@ main(int argc, char **argv)
 	chip8_init(&state, argv[1]);
 
 	SDL_Init(SDL_INIT_VIDEO);
-	win = SDL_CreateWindow("CHIP8", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	win = SDL_CreateWindow("CHIP-8", 100, 100, 640, 320, SDL_WINDOW_SHOWN);
 	if (win == NULL) {
 		SDL_Quit();
 		errx(1, "SDL_CreateWindow: %s", SDL_GetError());
@@ -47,6 +48,8 @@ main(int argc, char **argv)
 	}
 
 	for (;;) {
+		uint16_t opcode;
+
 		while (SDL_PollEvent(&ev)) {
 			if (ev.type == SDL_QUIT)
 				goto cleanup;
@@ -54,11 +57,13 @@ main(int argc, char **argv)
 			/* Set Keys */
 		}
 
-		/* Emulate Cycle */
+		opcode = *(uint16_t *)(state.memory + state.pc);
+		state.pc += 2;
 
-		/* Draw Cycle */
-		SDL_RenderClear(ren);
-		SDL_RenderPresent(ren);
+		printf("%#06X\n", opcode);
+
+		if (opcode == 0x00e0)
+			SDL_RenderClear(ren);
 	}
 
 cleanup:
@@ -86,7 +91,7 @@ const static uint8_t chip8_fontset[80] = {
 	0xF0,	0x80,	0xF0,	0x80,	0x80
 };
 
-void
+static void
 chip8_init(struct chip8_state *state, const char *path)
 {
 	FILE *fp;
@@ -101,4 +106,26 @@ chip8_init(struct chip8_state *state, const char *path)
 
 	fread(&(state->memory[0x200]), sizeof(uint8_t), 3136, fp);
 	fclose(fp);
+}
+
+static void
+chip8_draw(struct chip8_state *state, SDL_Renderer *ren)
+{
+	int x, y;
+	SDL_Rect r;
+
+	r.w = 10;
+	r.h = 10;
+
+	for (x = 0; x < 64; ++x) {
+		for (y = 0; y < 32; ++y) {
+			if (state->gfx[(32 * x) + y] == 0)
+				continue;
+
+			r.x = x * 10;
+			r.y = y * 10;
+
+			SDL_RenderFillRect(ren, &r);
+		}
+	}
 }
